5b11319100d6addfe39bde923b4e13f5d0fd88e1
Are1PM <arwanpriantomangidi@gmail.com>
2020-09-21 18:42:12 +0800

    ganti-nama-file

--- user_perlengkapan/export.php
+++ user_perlengkapan/export.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 if (isset($_SESSION['username']) && isset($_SESSION['akses']) && ($_SESSION['akses'] == 'perlengkapan')) {
-     if ($_GET['file'] == "pdf") {
-          include "export_pdf_bro.php";
-     } elseif ($_GET['file'] == "stok-pdf") {
-          include "export_stok_pdf.php";
+     if ($_GET['file'] == "bp-pdf") {
+          include "export_bp_pdf.php";
+     } elseif ($_GET['file'] == "tp-pdf") {
+          include "export_tp_pdf.php";
      } else {
           echo "<h1>Dokumen Tidak Ditemukan!</h1>";
      }
